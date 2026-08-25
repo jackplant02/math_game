2a1d23a2043afd0be00bc026354fffd72083013e
Jack Plant <91396233+jackplant02@users.noreply.github.com>
2026-08-25 11:08:57 -0400

    Simple math game

---
+++ math_game.py
@@ -0,0 +1,27 @@
+# Initialize score and done bools
+
+score = 0
+done = [False] * 8
+
+while True:
+    print("Please choose a question (1-8). Press 's' to see your current score. Press 'q' to quit. ")
+    selection = input().lower().strip()
+
+    if selection.startswith('s'):
+        print(f"\nCurrent score: {score}/8\n")
+        continue
+
+    if selection == 'q':
+        break
+
+    if isinstance(int(selection), int) & int(selection) in range(1, 9):
+        print()
+
+    else:
+        print("\nInvalid selection.")
+
+    if score == 8:
+        print("Congratulations! You have answered all of the questions correctly.")
+        break
+
+    
