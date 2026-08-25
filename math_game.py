cffbc4c48c9a86ac9a34e7438a952c307da51695
Jack Plant <91396233+jackplant02@users.noreply.github.com>
2026-08-25 15:48:58 -0400

    Work in progress

--- math_game.py
+++ math_game.py
@@ -14,8 +14,8 @@ while True:
     if selection == 'q':
         break
 
-    if isinstance(int(selection), int) & int(selection) in range(1, 9):
-        print()
+    if selection.isdigit() and int(selection) in range(1, 9):
+        print(f"\nQuestion {selection} is currently under construction.")
 
     else:
         print("\nInvalid selection.")
